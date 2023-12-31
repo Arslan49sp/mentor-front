8d753a86ed7a5e36ef2fdaebdd82d6ba80e405a5
Arslan Majeed <arslanmajeed214@gmail.com>
2023-12-31 11:24:10 +0500

    design modal

--- src/components/AddClassModel.tsx
+++ src/components/AddClassModel.tsx
@@ -1,51 +1,86 @@
-const AddClassModel = () => {
+import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Description should at least 2 characters" })
+    .max(50, {
+      message: "Description should contain maximum of 50 characters",
+    }),
+});
+
+type ClassFormData = z.infer<typeof schema>;
+
+interface Props {
+  showModal: boolean;
+  handleClose: () => void;
+  onSubmit: (data: ClassFormData) => void;
+}
+const AddClassModel = ({ onSubmit, showModal, handleClose }: Props) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleShow = () => {
+    if (modalRef.current) {
+      window.$(modalRef.current).modal("show");
+    }
+  };
+
+  handleShow();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<ClassFormData>({ resolver: zodResolver(schema) });
   return (
     <div
-      className="modal fade"
+      className={`modal fade ${showModal ? "show" : ""}`}
+      style={{ display: showModal ? "block" : "none" }}
       id="staticBackdrop"
       data-bs-backdrop="static"
       data-bs-keyboard="false"
       tabIndex={-1}
       aria-labelledby="staticBackdropLabel"
-      aria-hidden="true"
+      aria-hidden={!showModal}
     >
       <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title" id="staticBackdropLabel">
-              Modal title
+              Add new class
             </h5>
             <button
               type="button"
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
-              onClick={() => console.log("closing the model")}
+              onClick={handleClose}
             ></button>
           </div>
           <div className="modal-body">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
-              iste magni hic nobis, accusamus illo quo excepturi? Voluptatibus,
-              atque magnam. Tenetur, aut sunt laboriosam non voluptates repellat
-              totam, in magnam eos, aliquid similique at? Aperiam dolores saepe
-              et facilis! Corrupti quasi quibusdam quam pariatur in. Vel
-              architecto sint atque laudantium nam eos itaque quam, vero
-              provident incidunt deserunt non modi sed fugit obcaecati nostrum.
-              Sequi commodi voluptas fugiat eaque velit.
-            </p>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              data-bs-dismiss="modal"
+            <form
+              onSubmit={handleSubmit((data) => {
+                onSubmit(data);
+                reset();
+              })}
             >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary">
-              Understood
-            </button>
+              <div className="mb-3">
+                <label htmlFor="name">Name</label>
+                <input
+                  {...register("name")}
+                  id="name"
+                  type="text"
+                  className="form-control"
+                />
+                {errors.name && (
+                  <p className="text-danger">{errors.name.message} </p>
+                )}
+              </div>
+              <button className="btn btn-primary">Submit</button>
+            </form>
           </div>
         </div>
       </div>
--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,21 +1,34 @@
 import ClassesTable from "../components/ClassesTable";
 import AddClassModel from "../components/AddClassModel";
+import { useState } from "react";
 
 const Classes = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const handleShow = () => {
+    setShowModal(true);
+  };
   return (
     <div className="p-5">
       <h1>Classes</h1>
       <hr />
       <button
-        type="button"
         className="btn btn-success mb-2"
         data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
+        onClick={handleShow}
       >
         Add new
       </button>
       <ClassesTable />
-      <AddClassModel />
+      <AddClassModel
+        showModal={showModal}
+        handleClose={handleClose}
+        onSubmit={(data) => console.log(data)}
+      />
     </div>
   );
 };
