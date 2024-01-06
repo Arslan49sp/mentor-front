a3a1ab04fbe9d7d9e61e77fd2c1e9bb25b0a8f80
Arslan Majeed <arslanmajeed214@gmail.com>
2024-01-07 02:35:30 +0500

    Delete chapters

--- src/components/ChaptersTable.tsx
+++ src/components/ChaptersTable.tsx
@@ -1,53 +1,88 @@
 import { FaRegEye, FaPencil, FaTrash } from "react-icons/fa6";
-import useChapters from "../hooks/useChapters";
+import useChapters, { ChapterRes } from "../hooks/useChapters";
+import { useState } from "react";
+import { baseUrl } from "../data/api";
+import useDelete from "../hooks/useDelete";
+import ErrorToast from "./ErrorToast";
+import DeleteModal from "./DeleteModal";
 
 interface Props {
   subjectId: number;
 }
 const ChaptersTable = ({ subjectId }: Props) => {
   const { data, isLoading, error } = useChapters(subjectId);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [currentId, setCurrentId] = useState<number>(0);
+  const [showToast, setShowToast] = useState(true);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const handleClose = () => {
+    setShowDeleteModal(false);
+  };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const mutation = useDelete<ChapterRes>(handleClose, currentId, [
+    "subject",
+    subjectId,
+    "chapters",
+  ]);
 
-  if (!data) {
-    return null;
-  }
+  const handleDelete = (id: number) => {
+    setShowToast(true);
+    const url = baseUrl + "/chapters/" + id;
+    mutation.mutate(url);
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <table className="table table-bordered ">
-      <thead>
-        <tr className="d-flex">
-          <th className="sr-width text-center">#</th>
-          <th className="flex-grow-1">Name</th>
-          <th className="text-center px-5">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.data.map((classI, index) => (
-          <tr key={classI.id} className="d-flex">
-            <td className="sr-width text-center">{index + 1}</td>
-            <td className="flex-grow-1">{classI.name}</td>
-            <td className="d-flex flex-nowrap gap-0">
-              <button className="btn btn-link text-success">
-                <FaRegEye size={23} />
-              </button>
-              <button className="btn btn-link text-success">
-                <FaPencil size={19} />
-              </button>
-              <button className="btn btn-link text-danger">
-                <FaTrash size={19} />
-              </button>
-            </td>
+    <>
+      {mutation.error && (
+        <ErrorToast
+          isShow={showToast}
+          handleClose={() => setShowToast(false)}
+          message={mutation.error.message}
+        />
+      )}
+      <table className="table table-bordered ">
+        <thead>
+          <tr className="d-flex">
+            <th className="sr-width text-center">#</th>
+            <th className="flex-grow-1">Name</th>
+            <th className="text-center px-5">Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.data.map((classI, index) => (
+            <tr key={classI.id} className="d-flex">
+              <td className="sr-width text-center">{index + 1}</td>
+              <td className="flex-grow-1">{classI.name}</td>
+              <td className="d-flex flex-nowrap gap-0">
+                <button className="btn btn-link text-success">
+                  <FaRegEye size={23} />
+                </button>
+                <button className="btn btn-link text-success">
+                  <FaPencil size={19} />
+                </button>
+                <button
+                  className="btn btn-link text-danger"
+                  onClick={() => {
+                    setShowDeleteModal(true);
+                    setCurrentId(classI.id);
+                  }}
+                >
+                  <FaTrash size={19} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <DeleteModal
+        isShow={showDeleteModal}
+        handleClose={handleClose}
+        handleDelete={() => handleDelete(currentId)}
+      />
+    </>
   );
 };
 
