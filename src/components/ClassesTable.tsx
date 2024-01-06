d9fadcd457cd56ae7d4496d276a3524b866e20d3
Arslan Majeed <arslanmajeed214@gmail.com>
2024-01-06 12:35:32 +0500

    design delete modal

--- src/components/ClassesTable.tsx
+++ src/components/ClassesTable.tsx
@@ -1,50 +1,64 @@
+import { useState } from "react";
 import useClasses from "../hooks/useClasses";
 import { FaRegEye, FaPencil, FaTrash } from "react-icons/fa6";
+import DeleteModal from "./DeleteModal";
 
 const ClassesTable = () => {
   const { data, isLoading, error } = useClasses();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const handleClose = () => {
+    setShowDeleteModal(false);
+  };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const handleShow = () => {
+    setShowDeleteModal(true);
+  };
 
-  if (!data) {
-    return null;
-  }
+  const handleDelete = () => {};
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <table className="table table-bordered ">
-      <thead>
-        <tr className="d-flex">
-          <th className="sr-width text-center">#</th>
-          <th className="flex-grow-1">Name</th>
-          <th className="text-center px-5">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.data.map((classI, index) => (
-          <tr key={classI.id} className="d-flex">
-            <td className="sr-width text-center">{index + 1}</td>
-            <td className="flex-grow-1">{classI.name}</td>
-            <td className="d-flex flex-nowrap gap-0">
-              <button className="btn btn-link text-success">
-                <FaRegEye size={23} />
-              </button>
-              <button className="btn btn-link text-success">
-                <FaPencil size={19} />
-              </button>
-              <button className="btn btn-link text-danger">
-                <FaTrash size={19} />
-              </button>
-            </td>
+    <>
+      <table className="table table-bordered ">
+        <thead>
+          <tr className="d-flex">
+            <th className="sr-width text-center">#</th>
+            <th className="flex-grow-1">Name</th>
+            <th className="text-center px-5">Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.data.map((classI, index) => (
+            <tr key={classI.id} className="d-flex">
+              <td className="sr-width text-center">{index + 1}</td>
+              <td className="flex-grow-1">{classI.name}</td>
+              <td className="d-flex flex-nowrap gap-0">
+                <button className="btn btn-link text-success">
+                  <FaRegEye size={23} />
+                </button>
+                <button className="btn btn-link text-success">
+                  <FaPencil size={19} />
+                </button>
+                <button
+                  className="btn btn-link text-danger"
+                  onClick={handleDelete}
+                >
+                  <FaTrash size={19} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <DeleteModal
+        isShow={showDeleteModal}
+        handleClose={handleClose}
+        handleDelete={handleDelete}
+      />
+    </>
   );
 };
 
---
+++ src/components/DeleteModal.tsx
@@ -0,0 +1,32 @@
+import { Modal } from "react-bootstrap";
+
+interface Props {
+  isShow: boolean;
+  handleClose: () => void;
+  handleDelete: () => void;
+}
+const DeleteModal = ({ isShow, handleClose, handleDelete }: Props) => {
+  return (
+    <Modal
+      show={isShow}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Modal title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this</Modal.Body>
+      <Modal.Footer>
+        <button className="btn btn-danger" onClick={handleDelete}>
+          Yes
+        </button>
+        <button className="btn btn-success" onClick={handleClose}>
+          No
+        </button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
